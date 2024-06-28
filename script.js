a81cda153e11157886c80cfb568eff5dedc98fba
xhide341 <shawnehgn10@gmail.com>
2024-06-28 17:35:02 +0800

    add button hover color feature

--- script.js
+++ script.js
@@ -3,28 +3,40 @@ const toggleThemeCheckbox = document.getElementById('toggle-theme');
 const displayScreen = document.querySelector('.display-screen');
 const sunIcon = document.querySelector('.label-sun');
 const moonIcon = document.querySelector('.label-moon');
+const button = document.querySelectorAll('button');
 const operatorButtons = document.querySelectorAll('.button-operator');
 const numButtons = document.querySelectorAll('.button-num');
 
 function toggleButtonColors(isDarkMode) {
   if (isDarkMode) {
     numButtons.forEach(button => {
-      button.style.backgroundColor = '#283345';
-      button.style.color = '#fff';
+        button.style.backgroundColor = '#283345';
+        button.style.color = '#fff';
     });
     operatorButtons.forEach(button => {
         button.style.backgroundColor = '#283345'; // Adjust background color as needed
-      });
+    });
   } else {
-    // Light mode styles for numButtons
     numButtons.forEach(button => {
-      button.style.backgroundColor = '#eee';
-      button.style.color = '#000'; 
+        button.style.backgroundColor = '#eee';
+        button.style.color = '#000'; 
     });
     operatorButtons.forEach(button => {
         button.style.backgroundColor = '#eee'; // Adjust background color as needed
-      });
+    });
   }
+}
+
+function addHoverEffects(isDarkMode) {
+    button.forEach(button => {
+      if (isDarkMode) {
+        button.addEventListener('mouseover', () => button.style.backgroundColor = '#1d2838'); // Darker hover color
+        button.addEventListener('mouseout', () => button.style.backgroundColor = '#283345'); // Reset to original color
+      } else {
+        button.addEventListener('mouseover', () => button.style.backgroundColor = '#ddd'); // Lighter hover color
+        button.addEventListener('mouseout', () => button.style.backgroundColor = '#eee'); // Reset to original color
+      }
+    });
 }
 
 toggleThemeCheckbox.addEventListener('change', function() {
@@ -36,6 +48,9 @@ toggleThemeCheckbox.addEventListener('change', function() {
   displayScreen.style.backgroundColor = isDarkMode ? '#283345' : '#eee';
   displayScreen.style.color = isDarkMode ? '#eee' : '#000';
 
+
   // Toggle button colors using the function
   toggleButtonColors(isDarkMode);
+  addHoverEffects(isDarkMode);
 });
+
